c47b07b533d2744f63ddddfb13cb342e811137c3
Sadness <rosalimavictor@gmail.com>
2025-01-24 13:34:08 -0300

    0.0.0.0

--- main.go
+++ main.go
@@ -1,21 +1,71 @@
 package main
 
 import (
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/likexian/whois"
+	"github.com/likexian/whois-parser"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type DomainInfo struct {
+	Domain       string `json:"domain"`
+	CreatedDate  string `json:"created_date"`
+	IsLessThan6M bool   `json:"is_less_than_6_months"`
+	Error        string `json:"error,omitempty"`
+}
+
+func checkDomainAge(w http.ResponseWriter, r *http.Request) {
+	domain := r.URL.Query().Get("domain")
+	if domain == "" {
+		http.Error(w, "Missing 'domain' parameter", http.StatusBadRequest)
+		return
+	}
+
+	result := DomainInfo{Domain: domain}
+
+	// Perform WHOIS lookup
+	rawWhois, err := whois.Whois(domain)
+	if err != nil {
+		result.Error = fmt.Sprintf("Failed to fetch WHOIS: %v", err)
+		json.NewEncoder(w).Encode(result)
+		return
+	}
+
+	// Parse the WHOIS data
+	parsedWhois, err := whoisparser.Parse(rawWhois)
+	if err != nil {
+		result.Error = fmt.Sprintf("Failed to parse WHOIS data: %v", err)
+		json.NewEncoder(w).Encode(result)
+		return
+	}
+
+	// Extract the creation date
+	createdDate := parsedWhois.Domain.CreatedDate
+	result.CreatedDate = createdDate
+
+	// Check if the domain is less than 6 months old
+	creationTime, err := time.Parse("2006-01-02T15:04:05Z", createdDate)
+	if err != nil {
+		result.Error = fmt.Sprintf("Failed to parse creation date: %v", err)
+		json.NewEncoder(w).Encode(result)
+		return
+	}
+
+	sixMonthsAgo := time.Now().AddDate(0, -6, 0)
+	result.IsLessThan6M = creationTime.After(sixMonthsAgo)
+
+	// Return the result as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	http.HandleFunc("/check-domain", checkDomainAge)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	fmt.Println("Server is running on http://0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
